0997f036063558a6a702e1356844c92a905cc7cc
HajarDWM <Hajar.phy@gmail.com>
2025-03-26 12:29:38 +0000

    Add contact page and controller for user inquiries

---
+++ src/main/java/ma/enset/gestionconsultation/web/ContactController.java
@@ -0,0 +1,11 @@
+package ma.enset.gestionconsultation.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class ContactController {
+    @GetMapping("/contact")
+    public String showContactPage() {
+        return "contact";
+    }
+}
